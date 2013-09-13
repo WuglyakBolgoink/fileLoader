142b00f5d799dc97ff41e1fde3e4d356b2371ed3
Elderov Ali <cyberkatze@yandex.ru>
2013-09-13 13:32:45 +0200

    add ajax loader.. nicht fertig.. test mit local file - fail

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,8 @@ var fileSystem, // file system on Phone
     appStorage = window.localStorage,
     nDCcoreFile, // URL for local core file on FileSystem or in www-folder
 //    appName = "FileLoader.app", // application name
-    default_nDC_coreJSON_FilePath = "file://" + fileSystem.root.fullPath + "../www/js/defaultCore.json"; // default path to defaultCore.json in www-folder on Phone
+//    default_nDC_coreJSON_FilePath = "file://" + fileSystem.root.fullPath + "../www/js/defaultCore.json"; // default path to defaultCore.json in www-folder on Phone
+    default_nDC_coreJSON_FilePath = "js/defaultCore.json"; // default path to defaultCore.json in www-folder on Phone
 
 
 // fileError message
@@ -146,38 +147,65 @@ function onLoad() {
     } else {
         nDCcoreFile = default_nDC_coreJSON_FilePath;
     }
+    loadDefaultCore(nDCcoreFile);
 }
 // ------------------------------------------------------------------------------------------------------------------------
+// implement JSON.stringify serialization
+JSON.stringify = JSON.stringify || function (obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+        // simple data type
+        if (t == "string") {
+            obj = '"' + obj + '"';
+        }
+        return String(obj);
+    } else {
+        // recurse array or object
+        var n, v, json = [], arr = (obj && obj.constructor == Array);
+        for (n in obj) {
+            v = obj[n];
+            t = typeof (v);
+            if (t == "string") {
+                v = '"' + v + '"';
+            }
+            else if (t == "object" && v !== null) {
+                v = JSON.stringify(v);
+            }
+            json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
+};
+
+
 /**
- * open File and return it content
+ * open File and return it content, if error function return string "BAD"
  *
  * @param url - local/external
  * @param contentType - text/json/html/jsonp
  * @returns {string} - content
  */
 function openFile(url, contentType) {
+    echo("open url: " + url);
     var res = "";
     $.ajax({
         async: false,
         url: url,
         type: "GET",
         dataType: contentType,
-        error: function (err) {
-            alert("coreERROR: network error!");
-            echo(err);
+        error: function (e) {
+            res = "BAD" + JSON.stringify(e);
         },
         success: function (result) {
             res = result;
         }
     });//ajax
-
     return res;
 }
 
 function loadDefaultCore(url) {
-    console.log("versuche öffnen:", url);
     var tmp = openFile(url, "json");
-    if (tmp) {
+    if (tmp != "BAD") {
 //        readlocalFile(url);
         coreDefault = tmp;
     } else {
@@ -186,62 +214,46 @@ function loadDefaultCore(url) {
 }
 
 function loadServerCore(url) {
-    coreServer = openFile(url);
+    var tmp = openFile(url, "json");
+    if (tmp != "BAD") {
+        coreServer = tmp;
+    } else {
+        alert('can not load default template');
+    }
 }
 
-
-
-function getTemplate(url) {
-    var res = "";
-    $.ajax({
-        async: false,
-        url: url,
-        type: "GET",
-        dataType: 'html',
-        error: function () {
-            logit("templateERROR: network error! " + url);
-            res = 'BAD';
-        },
-        success: function (result) {
-            if (result) {
-                res = result;
-            } else {
-                logit("templateERROR: file not found! " + url);
-                res = 'BAD';
-            }
-        }
-    });//ajax
-
-    return res;
-}
 /**
  *
  * @param file - filename
  * @param url - urlname
  * @param index - index of item
  * @param newVersion - new version nummer
+ * @param path - path to template
  */
-function save2FS(file, url, index, newVersion) {
+function save2FS(file, url, index, newVersion, path) {
     fileSystem.root.getFile(file, {create: true}, function(f) {
-        var str = getTemplate(url);
+        var str = openFile(url, "text");
         if (str != "BAD") {
+
             coreDefault.nDC_CORE.pages[index].version = newVersion;
+            coreDefault.nDC_CORE.pages[index].path = path;
+
             f.createWriter(function(writerOb) {
-                writerOb.onwrite = function() {
-                    saveJSON('appCore.json', coreDefault);
+                writerOb.onwriteend = function() {
+                    saveJSON("appCore.json", coreDefault);
                 };
                 writerOb.write(str);
             });
         } // #if
     }, fileErrorMSG);
-
 }
 
 function saveJSON(fileIn, jsonIn) {
     fileSystem.root.getFile(fileIn, {create: true}, function(f) {
         f.createWriter(function(writerOb) {
             writerOb.onwrite = function() {
-                savePathToStorage('appCore.json');
+                var lPath = "file://" + fileSystem.root.fullPath + "/appCore.json";
+                savePathToStorage(lPath);
             };
             writerOb.write(JSON.stringify(jsonIn));
         });
@@ -256,8 +268,6 @@ function saveJSON(fileIn, jsonIn) {
  * @param ndcURL - json url
  */
 function compareCore(ndcURL) {
-    getById("#content").innerHTML = "";
-
     loadDefaultCore(nDCcoreFile);
     loadServerCore(ndcURL);
 
@@ -267,18 +277,20 @@ function compareCore(ndcURL) {
 
     var i = 0,
         j1 = coreDefault.nDC_CORE.pages,
-        j2 = coreServer.pages,
+        j2 = coreServer.nDC_CORE.pages,
         url = "",
         file = "",
-        dir = "";
+        dir = "",
+        path = "";
 
     for (i in j1) {
         if (j1[i].page == j2[i].page && parseFloat(j1[i].version) < parseFloat(j2[i].version)) {
             dir = j2[i].folder;
             file = j2[i].file;
-            url = coreServer.url + "/" + dir + "/" + file;
+            url = coreServer.nDC_CORE.url + "/" + dir + "/" + file;
+            path = "file://" + fileSystem.root.fullPath + "/" + file;
             //save "file" on FileSystem into root-Directory from "URL"
-            save2FS(file, url, i, j2[i].version);
+            save2FS(file, url, i, j2[i].version, path);
         } //#if/page
     } //#for/in
 }
@@ -290,7 +302,7 @@ $(document).on("click", "#loadJSON_default", function() {
 //    var tmp = openFile("http://ae.subsession.net/projects/nDC/lng.json", "json");
 //    echo(tmp);
     loadDefaultCore(nDCcoreFile);
-    logit(coreDefault);
+    echo(coreDefault);
 });
 
 $(document).on("click", "#loadJSON_server", function() {
@@ -298,22 +310,27 @@ $(document).on("click", "#loadJSON_server", function() {
     logit(coreServer);
 });
 
-
-
-
-
 $(document).on("click", "#clearStorage", function() {
     savePathToStorage(default_nDC_coreJSON_FilePath);
     console.log(">>> storage wurde entleert...");
 });
 
+function ttt(url) {
+    var s =  openFile(url, "text");
+    return s;
+}
+
+$(document).on("click", "#loadTemplate", function() {
+    echo("click loadTemplate:");
+    var url = coreDefault.nDC_CORE.pages[1].path + "/" + coreDefault.nDC_CORE.pages[1].file;
+    var t = ttt(url);
+    echo(url);
+    echo(t);
+    logit(t);
+});
 
 
-//$(document).on("click", "#loadTemplate", function() {
-//
-//});
 
 $(document).on("click", "#compareJSON", function() {
     compareCore('http://ae.subsession.net/projects/nDC/lng.json');
-    echo("...done...");
 }); // $(document).on("click", "#compareJSON"
